a6664fc76a17e39d6652fa0072fc0539b595f17e
shunnarski <ashunnarah9597@gmail.com>
2020-09-29 00:56:26 -0500

    removed secret

--- kringle/kringle/settings.py
+++ kringle/kringle/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'qcr9saxzqxbe&(*+s=@qcod35v7@tv_sf1jyd(dkyahsjpnf@4'
+SECRET_KEY = os.environ['DJANGO_SECRET']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
