0ca40e04eb46e84abbac68add4f28298f872fb78
shunnarski <ashunnarah9597@gmail.com>
2020-09-30 21:44:20 -0500

    Created navbar template with menu and page divisors

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,19 @@ import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { UserBioComponent } from './user-bio/user-bio.component';
 import { GiftListComponent } from './gift-list/gift-list.component';
+import { PageBodyComponent } from './page-body/page-body.component';
+import { MenuComponent } from './menu/menu.component';
+import { GiftEntryComponent } from './gift-entry/gift-entry.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
     UserBioComponent,
-    GiftListComponent
+    GiftListComponent,
+    PageBodyComponent,
+    MenuComponent,
+    GiftEntryComponent
   ],
   imports: [
     BrowserModule,
