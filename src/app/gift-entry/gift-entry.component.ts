21ce81ee07f1a1bc2fd22c5f63e4f424df135d0d
shunnarski <ashunnarah9597@gmail.com>
2020-10-05 22:30:15 -0500

    Started gift list creation

--- src/app/gift-entry/gift-entry.component.ts
+++ src/app/gift-entry/gift-entry.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { Gift } from '../gift';
 
 @Component({
   selector: 'app-gift-entry',
@@ -6,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./gift-entry.component.css']
 })
 export class GiftEntryComponent implements OnInit {
+
+  @Input() gift: Gift;
 
   constructor() { }
 
---
+++ src/app/mock-gifts.ts
@@ -0,0 +1,8 @@
+import {Gift} from './gift';
+
+export const GIFTS: Gift[] = [
+    {id: 1, name: "Alec", price: 100.0, link_url: "#", photo_url: "pic.jpg"},
+    {id: 2, name: "Rachael", price: 556.99, link_url: "#", photo_url: "pic.jpg"},
+    {id: 3, name: "Big Chungus 200 LB Sofa", price: 12.99, link_url: "#", photo_url: "pic.jpg"},
+    {id: 4, name: "Big ~Juicy~ Claws", price: 0.01, link_url: "#", photo_url: "pic.jpg"},
+]
