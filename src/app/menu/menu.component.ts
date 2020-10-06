90de07f7c67202f491271675ee15b8110e05f0a0
shunnarski <ashunnarah9597@gmail.com>
2020-10-05 19:25:36 -0500

    Added menu items to UI

--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { $ } from 'protractor';
 
 @Component({
   selector: 'app-menu',
@@ -6,10 +7,31 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./menu.component.css']
 })
 export class MenuComponent implements OnInit {
+  comp1 = {
+    name: "Gifts"
+  }
 
-  constructor() { }
+  comp2 = {
+    name: "Profile"
+  }
+
+  comp3 = {
+    name: "Settings"
+  }
+
+  menu_comps = [this.comp1, this.comp2, this.comp3];
+
+  selectedComponent = this.comp1;
+  
+  constructor() { 
+    
+    
+  }
 
   ngOnInit(): void {
   }
 
+  onSelect(menu_comp): void {
+    this.selectedComponent = menu_comp;
+  }
 }
